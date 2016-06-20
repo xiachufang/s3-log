c7d03dbe880572d91623e6fe963e75816f5993f2
Alex.F <gfreezy@gmail.com>
2016-06-20 10:10:18 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,33 @@
 # coding: utf-8
-
-
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
 
-here = path.abspath(path.dirname(__file__))
+setup(
+    name='s3_log',
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+    version='0.0.3',
 
-    setup(
-        name='s3_log',
+    description='s3 log utils',
+    long_description='s3 log utils',
 
-        version='0.0.2',
+    url='https://github.com/xiachufang/s3-log',
 
-        description='s3 log utils',
-        long_description=long_description,
+    # Author details
+    author='Kebin',
+    author_email='kebin@xiachufang.com',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
 
-        url='https://github.com/xiachufang/s3-log',
-
-        # Author details
-        author='Kebin',
-        author_email='kebin@xiachufang.com',
-        packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-
-        license='MIT',
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'License :: OSI Approved :: MIT License',
-            'Natural Language :: Chinese (Simplified)',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4',
-        ],
-        install_requires=['pytz', 'boto3'],
-        keywords='s3 log',
-        include_package_data=True
-    )
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    install_requires=['pytz', 'boto3'],
+    keywords='s3 log',
+    include_package_data=True
+)
